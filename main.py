bf30e0f95cd928807aef00aa28b2d9fd809a03f8
dhild <djh6278@psu.edu>
2020-08-28 05:49:18 +0000

    decimals weren't working so I added a float and got rid of int

--- main.py
+++ main.py
@@ -1,3 +1,2 @@
-Temperature = input("Enter temperature in celsius: ")
-Temperature = int(Temperature)
-print(f"{Temperature} in Celsius is equivalent to {Temperature * 1.8 + 32} Fahrenheit.")
+Temperature = float(input("Enter temperature in celsius: "))
+print(f"{Temperature}° in Celsius is equivalent to {Temperature * 1.80 + 32.0}° Fahrenheit.")
